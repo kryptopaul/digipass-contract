5d46761ca4c35987866e47dffca1ea8deeccf8d9
kryptopaul <pl4249t@gre.ac.uk>
2023-12-08 21:51:10 +0000

    final

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Digipass} from "../src/Digipass.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
-
+contract DeployScript is Script {
     function run() public {
-        vm.broadcast();
+
     }
 }
--- src/Digipass.sol
+++ src/Digipass.sol
@@ -1,29 +1,154 @@
-pragma solidity 0.8.21;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
-import "ERC721A/ERC721A.sol";
+import "ERC721A/extensions/ERC721AQueryable.sol";
+import {FunctionsClient} from "chainlink/src/v0.8/functions/dev/v1_X/FunctionsClient.sol";
+import {ConfirmedOwner} from "chainlink/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "chainlink/src/v0.8/functions/dev/v1_X/libraries/FunctionsRequest.sol";
 
-contract Digipass is ERC721A {
+contract Digipass is ERC721AQueryable, FunctionsClient, ConfirmedOwner {
+    using FunctionsRequest for FunctionsRequest.Request;
+    // State variables to store the last request ID, response, and error
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+
+    // Custom error type
+    error UnexpectedRequestID(bytes32 requestId);
+
+    // Event to log responses
+    event Response(
+        bytes32 indexed requestId,
+        string price,
+        bytes response,
+        bytes err
+    );
+
+    // Router address - Hardcoded for Mumbai
+    // Check to get the router address for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+    address router = 0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0;
+    // JavaScript source code
+    // Fetch character name from the Star Wars API.
+    // Documentation: https://swapi.dev/documentation#people
+    string source =
+        "const price = args[0];"
+        "const apiResponse = await Functions.makeHttpRequest({"
+        "url: `https://digipass-ui.vercel.app/api/price?destination=${price}`"
+        "});"
+        "if (apiResponse.error) {"
+        "throw Error('Request failed');"
+        "}"
+        "const { data } = apiResponse;"
+        "return Functions.encodeString(data.price);";
+
+    //Callback gas limit
+    uint32 gasLimit = 300000;
+
+    // donID - Hardcoded for Mumbai
+    // Check to get the donID for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+    bytes32 donID =
+        0x66756e2d6176616c616e6368652d66756a692d31000000000000000000000000;
+
+    // State variable to store the returned character information
+    string public price;
+
+    constructor()
+        ERC721A("Digipass", "DGP")
+        FunctionsClient(router)
+        ConfirmedOwner(msg.sender)
+    {}
+
+    /**
+     * @notice Sends an HTTP request for character information
+     * @param subscriptionId The ID for the Chainlink subscription
+     * @param args The arguments to pass to the HTTP request
+     * @return requestId The ID of the request
+     */
+    function sendRequest(
+        uint64 subscriptionId,
+        string[] calldata args,
+        string calldata imageUri
+    ) external returns (bytes32 requestId) {
+        if (balanceOf(msg.sender) == 0) revert();
+        FunctionsRequest.Request memory req;
+        req._initializeRequestForInlineJavaScript(source); // Initialize the request with JS code
+        if (args.length > 0) req._setArgs(args); // Set the arguments for the request
+
+        // Send the request and store the request ID
+        s_lastRequestId = _sendRequest(
+            req._encodeCBOR(),
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+
+        uint256 targetToken = this.tokensOfOwner(msg.sender)[0];
+        travels.push(
+            Travel({
+                chainlinkId: s_lastRequestId,
+                tokenId: targetToken,
+                timestamp: block.timestamp,
+                price: "",
+                destination: args[0],
+                uri: imageUri
+            })
+        );
+
+        return s_lastRequestId;
+    }
+
+    /**
+     * @notice Callback function for fulfilling a request
+     * @param requestId The ID of the request to fulfill
+     * @param response The HTTP response data
+     * @param err Any errors from the Functions request
+     */
+    function _fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal override {
+        if (s_lastRequestId != requestId) {
+            revert UnexpectedRequestID(requestId); // Check if request IDs match
+        }
+        // Update the contract's state variables with the response and any errors
+        s_lastResponse = response;
+        price = string(response);
+        s_lastError = err;
+        // Find travel with a given id and update price param to string(response)
+        for (uint256 i = 0; i < travels.length; i++) {
+            if (travels[i].chainlinkId == requestId) {
+                travels[i].price = price;
+            }
+        }
+
+        // Emit an event to log the response
+        emit Response(requestId, price, s_lastResponse, s_lastError);
+    }
+
     struct Travel {
+        bytes32 chainlinkId;
+        uint256 tokenId;
         uint256 timestamp;
-        uint256 price;
+        string price;
         string destination;
         string uri;
     }
-    string url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-55yTuhzoSimH60B85vEr7HlA/user-g3GFdTthyDz77tUBH2n6lmnS/img-rPH4BrOzvtT81vprZkLiUD9F.png?st=2023-12-07T18%3A17%3A31Z&se=2023-12-07T20%3A17%3A31Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-07T16%3A43%3A15Z&ske=2023-12-08T16%3A43%3A15Z&sks=b&skv=2021-08-06&sig=dFxCoMczOJH/W2Zc8qwYqJbCrfNKGXZoWsxefMqPIpA%3D";
 
-    mapping(uint256 => Travel[]) travels;
+    Travel[] public travels;
 
-    constructor() ERC721A("Digipass", "DP") {}
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override(ERC721A, IERC721A) returns (string memory) {
+        return "https://digipass-ui.vercel.app/api/metadata";
+    }
 
     function mint() external payable {
         if (_numberMinted(msg.sender) > 0) revert();
         _mint(msg.sender, 1);
     }
 
-    function assignTravel(uint256 tokenId, uint256 price, string memory destination, string memory uri) external {
-        travels[tokenId].push(Travel(block.timestamp, price, destination, uri));
+    function getTravels() external view returns (Travel[] memory) {
+        return travels;
     }
-
-
 }
-
